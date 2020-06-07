929c4a7abc49d58dc44e862cd9144109d20b2f9c
huzhiyuan <1714581719@qq.com>
2020-06-07 17:47:22 +0800

    登录注册css完成

---
+++ src/script/loginHeader.js
@@ -0,0 +1,7 @@
+!function($) {
+    $('.commonHeader').load("http://10.31.162.21/ZOL/src/header.html",function() {
+        $('.navs').css({
+            display:'none'
+        });
+    });
+}(jQuery);
