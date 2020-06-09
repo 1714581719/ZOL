ea3870e9aa06e8219d159b9420c368c0d706cd01
huzhiyuan <1714581719@qq.com>
2020-06-09 19:26:12 +0800

    详情页放大镜缩略图完成

--- src/script/detail.js
+++ src/script/detail.js
@@ -1,3 +1,165 @@
+// 渲染
 !function($) {
-    
+    // 获取传来的sid
+    let $sid = location.search.substring(1).split('=')[1];
+
+    // 设置默认sid
+    if(!$sid) {
+        $sid = 4;
+    }
+
+    $.ajax({
+        url: 'http://10.31.162.21/ZOL/php/gitsid.php',
+        data: {
+            sid: $sid
+        },
+        dataType: 'json'
+    }).done(function(data) {
+        $('.title').html(data.goods_name);
+        $('.price i').html(data.goods_price);
+        // 设置大图的图片路径
+        $('.imgbox img').attr("src",data.goods_img);
+
+        let $picarr = data.piclistimg.split(',');
+        let $strhtml = `
+            <li>
+                <img src="${data.goods_img}" alt="">
+            </li>
+        `;
+
+        // 设置常量
+        const $iconLeft = $('.icon-left1');
+        const $iconRight = $('.icon-right');
+        const $imgUl = $('.imglist ul');
+
+        for(let i = 0; i < $picarr.length; i++) {
+            $strhtml += `
+                <li>
+                    <img src="${$picarr[i]}" alt="">
+                </li>
+            `;
+        }
+        // 设置imgUl的宽度
+        $imgUl.css({
+            width: ($picarr.length + 1) * (64 + 14)
+        }).html($strhtml);
+
+        // 设置常量
+        const $imgUlList = $('.imglist ul li');
+
+        $index = 0;// 缩略图选中下标
+        // 缩略图效果
+        $imgUlList.eq($index).addClass('active');
+        // 鼠标点击左箭头事件
+        $iconLeft.on('click',function() {
+            $index--;
+            imgMove();
+        });
+
+        // 鼠标点击右箭头事件
+        $iconRight.on('click',function() {
+            $index++;
+            imgMove();
+        });
+
+        // 鼠标点击缩略图事件
+        $imgUlList.on('click',function() {
+            $index = $(this).index();
+            imgMove();
+        });
+
+        // 鼠标点击左右箭头时缩略图的变化
+        function imgMove() {
+            // 给左右按钮添加状态
+            if($index==0) {
+                $iconLeft.addClass('iconfont_ban');
+            }else if($index == $picarr.length) {
+                $iconRight.addClass('iconfont_ban');
+            }else {
+                $iconLeft.removeClass('iconfont_ban');
+                $iconRight.removeClass('iconfont_ban');
+            }
+            // 给li添加边框
+            $imgUlList.eq($index).addClass('active').siblings('.imglist ul li').removeClass('active');
+            //移动ul前判断是否移动    +1是因为自己额外添加了一张缩略图
+            if($index <= $picarr.length + 1 - 5){
+                // 移动ul
+                $imgUl.css({
+                    left: - $index * ($imgUlList.outerWidth() + 14)
+                });
+            }else {
+                $imgUl.css({
+                    left: - ($picarr.length + 1 - 5) * ($imgUlList.outerWidth() + 14)
+                });
+            }
+            // 设置大图的图片路径
+            if($index == 0) {
+                $('.imgbox img').attr("src",data.goods_img);
+            }else {
+                $('.imgbox img').attr("src",$picarr[$index-1]);
+            }
+        }
+    });
+}(jQuery);
+
+// 放大镜效果
+!function($) {
+    const $bigbox = $('.img_small');
+    const $img_small = $('.img_small img');
+    const $img_big = $('.img_big img');
+    const $xiaofang = $('.xiaofang');
+    const $dafang = $('.img_big');
+
+    //设置小放的尺寸
+    $xiaofang.css({
+        width: $bigbox.outerWidth() / $img_big.outerWidth() * $dafang.outerWidth(),
+        height: $bigbox.outerHeight() / $img_big.outerHeight() * $dafang.outerHeight()
+    });
+
+    $bigbox.hover(function() {
+        // 显示小放和大放
+        $xiaofang.css({
+            visibility: 'visible'
+        });
+        $dafang.css({
+            visibility: 'visible'
+        });
+        
+        $(this).on('mousemove',function(ev) {
+            var ev = ev || window.event;
+            let leftvalue = ev.pageX - $bigbox.offset().left - $xiaofang.outerWidth() / 2;
+            let topvalue = ev.pageY - $bigbox.offset().top - $xiaofang.outerHeight() / 2;
+
+            // 规定小放的移动范围
+            if(leftvalue < 0) {
+                leftvalue = 0;
+            }else if(leftvalue > $bigbox.outerWidth() - $xiaofang.outerWidth() - 2) {
+                leftvalue = $bigbox.outerWidth() - $xiaofang.outerWidth() - 2;
+            }
+            if(topvalue < 0 ) {
+                topvalue = 0
+            }else if(topvalue > $bigbox.outerHeight() - $xiaofang.outerHeight() - 2) {
+                topvalue = $bigbox.outerHeight() - $xiaofang.outerHeight() - 2;
+            }
+
+            // 小图移动
+            $xiaofang.css({
+                left: leftvalue,
+                top: topvalue 
+            });
+
+            // 大图移动
+            $img_big.css({
+                left: - $dafang.outerWidth() / $xiaofang.outerWidth() * leftvalue,
+                top: - $dafang.outerHeight() / $xiaofang.outerHeight() * topvalue,
+            });
+        })
+    },function() {
+        $xiaofang.css({
+            visibility: 'hidden'
+        });
+        $dafang.css({
+            visibility: 'hidden'
+        });
+    });
 }(jQuery);
